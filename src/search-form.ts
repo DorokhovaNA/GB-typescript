37c14deae55a7705a4e49060436f5a2d3bc58a87
DorokhovaNA <nisan261lib@mail.ru>
2022-06-05 21:05:59 +0300

    refactor

--- src/search-form.ts
+++ src/search-form.ts
@@ -1,45 +1,47 @@
-import { renderBlock } from './lib.js';
+import { renderBlock } from "./lib.js";
 
-export function renderSearchFormBlock (dateFrom, dateTo) {
+export function renderSearchFormBlock(
+  dateFrom: string = null,
+  dateTo: string = null
+) {
   const minDate = new Date();
   const maxDate = new Date(minDate.getFullYear(), minDate.getMonth() + 2, 0);
-  
+
   let currentDateFrom = dateFrom;
   let currentDateTo = dateTo;
 
-  if ((dateFrom === null) ||
-      (Date.parse(dateFrom) < Date.parse(formatDate(minDate)))
-  ) {
+  if (Date.parse(dateFrom) < Date.parse(formatDate(minDate))) {
     const defaultDateFrom = new Date(minDate.setDate(minDate.getDate() + 1));
     currentDateFrom = formatDate(defaultDateFrom);
   }
 
   if (
-    (dateTo === null) ||
-    (Date.parse(dateTo) > Date.parse(formatDate(maxDate))) ||
-    (Date.parse(dateTo) < Date.parse(formatDate(minDate)))
+    Date.parse(dateTo) > Date.parse(formatDate(maxDate)) ||
+    Date.parse(dateTo) < Date.parse(formatDate(minDate))
   ) {
     const defaultDateFrom = new Date(dateFrom);
-    const defaultDateTo = new Date(defaultDateFrom.setDate(defaultDateFrom.getDate() + 2));
+    const defaultDateTo = new Date(
+      defaultDateFrom.setDate(defaultDateFrom.getDate() + 2)
+    );
     currentDateTo = formatDate(defaultDateTo);
   }
 
-  function formatDate (date) {
+  function formatDate(date) {
     let dd = date.getDate();
 
-    if (dd < 10) dd = '0' + dd;
-  
+    if (dd < 10) dd = "0" + dd;
+
     let mm = date.getMonth() + 1;
 
-    if (mm < 10) mm = '0' + mm;
-  
+    if (mm < 10) mm = "0" + mm;
+
     const yyyy = date.getFullYear();
-  
-    return yyyy + '-' + mm + '-' + dd;
+
+    return yyyy + "-" + mm + "-" + dd;
   }
 
   renderBlock(
-    'search-form-block',
+    "search-form-block",
     `
     <form>
       <fieldset class="search-filedset">
@@ -57,11 +59,25 @@ export function renderSearchFormBlock (dateFrom, dateTo) {
         <div class="row">
           <div>
             <label for="check-in-date">Дата заезда</label>
-            <input id="check-in-date" type="date" value=${currentDateFrom} min=${formatDate(minDate)} max="${formatDate(maxDate)}" name="checkin" />
+            <input
+              id="check-in-date"
+              type="date"
+              value=${currentDateFrom}
+              min=${formatDate(minDate)}
+              max="${formatDate(maxDate)}"
+              name="checkin"
+            />
           </div>
           <div>
             <label for="check-out-date">Дата выезда</label>
-            <input id="check-out-date" type="date" value=${currentDateTo} min=${formatDate(minDate)} max="${formatDate(maxDate)}" name="checkout" />
+            <input
+              id="check-out-date"
+              type="date"
+              value=${currentDateTo}
+              min=${formatDate(minDate)}
+              max="${formatDate(maxDate)}"
+              name="checkout"
+            />
           </div>
           <div>
             <label for="max-price">Макс. цена суток</label>
@@ -74,5 +90,5 @@ export function renderSearchFormBlock (dateFrom, dateTo) {
       </fieldset>
     </form>
     `
-  )
+  );
 }
